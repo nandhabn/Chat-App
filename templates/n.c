4269cb4f45e56b2b11b410ebb7b842e0cf6a1665
Nandhagopal <48519744+nandhubn@users.noreply.github.com>
2020-01-20 18:46:49 +0530

    Delete n.c

--- templates/n.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char *num="192323";
-    char *single_digits[] = { "zero", "one", "two","three", "four","five", "six", "seven", "eight", "nine"};
-    printf("%s\n", single_digits[*num - '0']);
-}
